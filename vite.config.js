92d6e17082505a5464751a2fb9c772517f9f4208
Chiragdbb <schirag414@gmail.com>
2023-03-25 03:38:38 +0530

    prod v0.9

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+export default ({
+    build: {
+      rollupOptions: {
+        input: {
+            base: './index.html',
+            main2: './main.js',
+            contact: './contact.html',
+            package: './package.html',
+            service: './service.html',
+            about: './about.html',
+        },
+    },
+},
+});
+
